2a6020b91a53867b19d9d3a90bb1a320b2b2ca34
Sebastian-Andersson <sebastian@cshift.tech>
2023-05-09 16:54:07 +0200

    had some headache but im fine now

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,11 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
-        </ul>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -23,6 +21,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
-
-
